f9cc5862e37757c97372734b814797b557a6273d
Jason <jsnmarek@gmail.com>
2013-10-09 17:07:37 -0500

    Initial upload

---
+++ build_master.sh
@@ -0,0 +1,110 @@
+#!/bin/sh
+export PLATFORM="AOSP"
+export MREV="JB4.3"
+export CURDATE=`date "+%m.%d.%Y"`
+export MUXEDNAMELONG="Slimmed-SGS4-$MREV-$PLATFORM-ATT-$CURDATE"
+export MUXEDNAMESHRT="Slimmed-SGS4-$MREV-$PLATFORM-ATT*"
+export SKVER="--$MUXEDNAMELONG--"
+export KERNELDIR=`readlink -f .`
+export PARENT_DIR=`readlink -f ..`
+export INITRAMFS_DEST=$KERNELDIR/kernel/usr/initramfs
+export INITRAMFS_SOURCE=`readlink -f ..`/RAMDISKS/AOSP_ATT4.3
+export CONFIG_$PLATFORM_BUILD=y
+export PACKAGEDIR=$PARENT_DIR/Packages/$PLATFORM
+export UPLOADER=/home/jason/Android/Projects/Dropbox-Uploader/
+#Enable FIPS mode
+export USE_SEC_FIPS_MODE=true
+export ARCH=arm
+# export CROSS_COMPILE=$PARENT_DIR/linaro4.5/bin/arm-eabi-
+# export CROSS_COMPILE=/home/ktoonsez/kernel/siyah/arm-2011.03/bin/arm-none-eabi-
+# export CROSS_COMPILE=/home/ktoonsez/android/system/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
+# export CROSS_COMPILE=/home/ktoonsez/aokp4.2/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-
+export CROSS_COMPILE=/home/jason/Toolchains/android-toolchain-eabi-4.7.4/bin/arm-eabi-
+
+time_start=$(date +%s.%N)
+
+echo "Remove old Package Files"
+rm -rf $PACKAGEDIR/*
+
+echo "Setup Package Directory"
+mkdir -p $PACKAGEDIR/system/app
+mkdir -p $PACKAGEDIR/system/lib/modules
+mkdir -p $PACKAGEDIR/system/etc/init.d
+
+echo "Create initramfs dir"
+mkdir -p $INITRAMFS_DEST
+
+echo "Remove old initramfs dir"
+rm -rf $INITRAMFS_DEST/*
+
+echo "Copy new initramfs dir"
+cp -R $INITRAMFS_SOURCE/* $INITRAMFS_DEST
+
+echo "chmod initramfs dir"
+chmod -R g-w $INITRAMFS_DEST/*
+rm $(find $INITRAMFS_DEST -name EMPTY_DIRECTORY -print)
+rm -rf $(find $INITRAMFS_DEST -name .git -print)
+
+echo "Remove old zImage"
+rm $PACKAGEDIR/zImage
+rm arch/arm/boot/zImage
+
+echo "Make the kernel"
+make VARIANT_DEFCONFIG=jf_ATT_defconfig SELINUX_DEFCONFIG=jfselinux_defconfig SELINUX_LOG_DEFCONFIG=jfselinux_log_defconfig Slimmed_jf_defconfig
+
+echo "Modding .config file - "$SKVER
+sed -i 's,CONFIG_LOCALVERSION="-Slimmed.Kernel",CONFIG_LOCALVERSION="'$SKVER'",' .config
+
+HOST_CHECK=`uname -n`
+if [ $HOST_CHECK = 'jason-pc' ]; then
+	echo "jason-pc/jsnmarek !!"
+	make -j12
+else
+	echo "Others! - " + $HOST_CHECK
+	make -j`grep 'processor' /proc/cpuinfo | wc -l`
+fi;
+
+echo "Copy modules to Package"
+cp -a $(find . -name *.ko -print |grep -v initramfs) $PACKAGEDIR/system/lib/modules/
+if [ $ADD_KTWEAKER = 'Y' ]; then
+	cp /home/jason/Android/kernel/com.ktoonsez.KTweaker.apk $PACKAGEDIR/system/app/com.ktoonsez.KTweaker.apk
+fi;
+
+if [ -e $KERNELDIR/arch/arm/boot/zImage ]; then
+	echo "Copy zImage to Package"
+	cp arch/arm/boot/zImage $PACKAGEDIR/zImage
+
+	echo "Make boot.img"
+	./mkbootfs $INITRAMFS_DEST | gzip > $PACKAGEDIR/ramdisk.gz
+	./mkbootimg --cmdline 'console = null androidboot.hardware=qcom user_debug=31 zcache' --kernel $PACKAGEDIR/zImage --ramdisk $PACKAGEDIR/ramdisk.gz --base 0x80200000 --pagesize 2048 --ramdisk_offset 0x02000000 --output $PACKAGEDIR/boot.img 
+	if [ $EXEC_LOKI = 'Y' ]; then
+		echo "Executing loki"
+		./loki_patch-linux-x86_64 boot abootATT.img $PACKAGEDIR/boot.img $PACKAGEDIR/boot.lok
+		rm $PACKAGEDIR/boot.img
+	fi;
+	cd $PACKAGEDIR
+	if [ $EXEC_LOKI = 'Y' ]; then
+		cp -R ../META-INF-SEC ./META-INF
+	else
+		cp -R ../META-INF .
+	fi;
+	rm ramdisk.gz
+	rm zImage
+	rm ../$MUXEDNAMESHRT.zip
+	zip -r ../$MUXEDNAMELONG.zip .
+
+	time_end=$(date +%s.%N)
+	echo -e "${BLDYLW}Total time elapsed: ${TCTCLR}${TXTGRN}$(echo "($time_end - $time_start) / 60"|bc ) ${TXTYLW}minutes${TXTGRN} ($(echo "$time_end - $time_start"|bc ) ${TXTYLW}seconds) ${TXTCLR}"
+
+	FILENAME=../$MUXEDNAMELONG.zip
+	FILESIZE=$(stat -c%s "$FILENAME")
+	echo "Size of $FILENAME = $FILESIZE bytes."
+
+	cd $KERNELDIR
+	echo "Upload zip"
+	./uploader.sh upload /home/jason/Android/kernel/Packages/$MUXEDNAMELONG.zip /SGS4/$PLATFORM/$MUXEDNAMELONG.zip
+	echo "File upload complete"
+
+else
+	echo "KERNEL DID NOT BUILD! no zImage exist"
+fi;
