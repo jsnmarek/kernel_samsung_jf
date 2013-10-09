43b0a95ffd95f246e64cb2c49ee227b3fdfb28d3
Jason <jsnmarek@gmail.com>
2013-10-09 12:02:51 -0500

    Fix upload path

--- build_master.sh
+++ build_master.sh
@@ -11,6 +11,7 @@ export INITRAMFS_DEST=$KERNELDIR/kernel/usr/initramfs
 export INITRAMFS_SOURCE=`readlink -f ..`/RAMDISKS/AOSP_ATT4.3
 export CONFIG_$PLATFORM_BUILD=y
 export PACKAGEDIR=$PARENT_DIR/Packages/$PLATFORM
+export UPLOADER=/home/jason/Android/Projects/Dropbox-Uploader
 #Enable FIPS mode
 export USE_SEC_FIPS_MODE=true
 export ARCH=arm
@@ -98,6 +99,8 @@ if [ -e $KERNELDIR/arch/arm/boot/zImage ]; then
 	FILENAME=../$MUXEDNAMELONG.zip
 	FILESIZE=$(stat -c%s "$FILENAME")
 	echo "Size of $FILENAME = $FILESIZE bytes."
+
+	$UPLOADER/.dropbox_uploader.sh upload $MUXEDNAMELONG.zip /SGS4/$PLATFORM
 
 	cd $KERNELDIR
 else
