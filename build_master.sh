db14dc2d448b1c0c96c6d663a920498b4f0c3bff
Jason <jsnmarek@gmail.com>
2013-10-14 11:39:46 -0500

    Update build script

--- build_master.sh
+++ build_master.sh
@@ -2,29 +2,27 @@
 export PLATFORM="AOSP"
 export MREV="JB4.3"
 export CURDATE=`date "+%m.%d.%Y"`
-export MUXEDNAMELONG="Slimmed-SGS4-$MREV-$PLATFORM-ATT-$CURDATE"
-export MUXEDNAMESHRT="Slimmed-SGS4-$MREV-$PLATFORM-ATT*"
-export SKVER="--$MUXEDNAMELONG--"
+export MUXEDNAMELONG="SlimmedKernel-$MREV-$PLATFORM-$CARRIER-$CURDATE"
+export MUXEDNAMESHRT="SlimmedKernel-$MREV-$PLATFORM-$CARRIER*"
+export KTVER="-$MUXEDNAMELONG"
+export SRC_ROOT=`readlink -f ../../..`
 export KERNELDIR=`readlink -f .`
 export PARENT_DIR=`readlink -f ..`
 export INITRAMFS_DEST=$KERNELDIR/kernel/usr/initramfs
-export INITRAMFS_SOURCE=`readlink -f ..`/RAMDISKS/AOSP_ATT4.3
+export INITRAMFS_SOURCE=`readlink -f ..`/RAMDISKS/$PLATFORM"_"$CARRIER-$MREV
 export CONFIG_$PLATFORM_BUILD=y
 export PACKAGEDIR=$PARENT_DIR/Packages/$PLATFORM
+# enable ccache
 export USE_CCACHE=1
 #Enable FIPS mode
 export USE_SEC_FIPS_MODE=true
 export ARCH=arm
-# export CROSS_COMPILE=$PARENT_DIR/linaro4.5/bin/arm-eabi-
-# export CROSS_COMPILE=/home/ktoonsez/kernel/siyah/arm-2011.03/bin/arm-none-eabi-
-# export CROSS_COMPILE=/home/ktoonsez/android/system/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
-# export CROSS_COMPILE=/home/ktoonsez/aokp4.2/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-
 export CROSS_COMPILE=/home/jason/Toolchains/android-toolchain-eabi-4.7.4/bin/arm-eabi-
 
 time_start=$(date +%s.%N)
 
 echo "Remove old Package Files"
-rm -rf $PACKAGEDIR/*
+rm -rf $PACKAGEDIR/* > /dev/null 2>&1
 
 echo "Setup Package Directory"
 mkdir -p $PACKAGEDIR/system/app
@@ -35,29 +33,29 @@ echo "Create initramfs dir"
 mkdir -p $INITRAMFS_DEST
 
 echo "Remove old initramfs dir"
-rm -rf $INITRAMFS_DEST/*
+rm -rf $INITRAMFS_DEST/* > /dev/null 2>&1
 
 echo "Copy new initramfs dir"
 cp -R $INITRAMFS_SOURCE/* $INITRAMFS_DEST
 
 echo "chmod initramfs dir"
 chmod -R g-w $INITRAMFS_DEST/*
-rm $(find $INITRAMFS_DEST -name EMPTY_DIRECTORY -print)
+rm $(find $INITRAMFS_DEST -name EMPTY_DIRECTORY -print) > /dev/null 2>&1
 rm -rf $(find $INITRAMFS_DEST -name .git -print)
 
 echo "Remove old zImage"
-rm $PACKAGEDIR/zImage
-rm arch/arm/boot/zImage
+rm $PACKAGEDIR/zImage > /dev/null 2>&1
+rm arch/arm/boot/zImage > /dev/null 2>&1
 
 echo "Make the kernel"
-make VARIANT_DEFCONFIG=jf_ATT_defconfig SELINUX_DEFCONFIG=jfselinux_defconfig SELINUX_LOG_DEFCONFIG=jfselinux_log_defconfig Slimmed_jf_defconfig
+make VARIANT_DEFCONFIG=jf_$CARRIER"_defconfig" SELINUX_DEFCONFIG=jfselinux_defconfig SELINUX_LOG_DEFCONFIG=jfselinux_log_defconfig Slimmed_jf_defconfig
 
-echo "Modding .config file - "$SKVER
-sed -i 's,CONFIG_LOCALVERSION="-Slimmed.Kernel",CONFIG_LOCALVERSION="'$SKVER'",' .config
+echo "Modding .config file - "$KTVER
+sed -i 's,CONFIG_LOCALVERSION="-Slimmed.Kernel",CONFIG_LOCALVERSION="'$KTVER'",' .config
 
 HOST_CHECK=`uname -n`
 if [ $HOST_CHECK = 'jason-pc' ]; then
-	echo "jason-pc/jsnmarek !!"
+	echo "jason-pc detected!! Running make with 12 jobs"
 	make -j12
 else
 	echo "Others! - " + $HOST_CHECK
@@ -66,7 +64,7 @@ fi;
 
 echo "Copy modules to Package"
 cp -a $(find . -name *.ko -print |grep -v initramfs) $PACKAGEDIR/system/lib/modules/
-if [ $ADD_KTWEAKER = 'Y' ]; then
+if [ $ADD_CHRONIC_CONFIG = 'Y' ]; then
 	cp /home/jason/Android/kernel/com.ktoonsez.KTweaker.apk $PACKAGEDIR/system/app/com.ktoonsez.KTweaker.apk
 fi;
 
@@ -79,7 +77,7 @@ if [ -e $KERNELDIR/arch/arm/boot/zImage ]; then
 	./mkbootimg --cmdline 'console = null androidboot.hardware=qcom user_debug=31 zcache' --kernel $PACKAGEDIR/zImage --ramdisk $PACKAGEDIR/ramdisk.gz --base 0x80200000 --pagesize 2048 --ramdisk_offset 0x02000000 --output $PACKAGEDIR/boot.img 
 	if [ $EXEC_LOKI = 'Y' ]; then
 		echo "Executing loki"
-		./loki_patch-linux-x86_64 boot abootATT.img $PACKAGEDIR/boot.img $PACKAGEDIR/boot.lok
+		./loki_patch-linux-x86_64 boot aboot$CARRIER.img $PACKAGEDIR/boot.img $PACKAGEDIR/boot.lok
 		rm $PACKAGEDIR/boot.img
 	fi;
 	cd $PACKAGEDIR
@@ -90,15 +88,17 @@ if [ -e $KERNELDIR/arch/arm/boot/zImage ]; then
 	fi;
 	rm ramdisk.gz
 	rm zImage
-	rm ../$MUXEDNAMESHRT.zip
+	rm ../$MUXEDNAMESHRT.zip > /dev/null 2>&1
 	zip -r ../$MUXEDNAMELONG.zip .
 
-	time_end=$(date +%s.%N)
+time_end=$(date +%s.%N)
 	echo -e "${BLDYLW}Total time elapsed: ${TCTCLR}${TXTGRN}$(echo "($time_end - $time_start) / 60"|bc ) ${TXTYLW}minutes${TXTGRN} ($(echo "$time_end - $time_start"|bc ) ${TXTYLW}seconds) ${TXTCLR}"
 
 	FILENAME=../$MUXEDNAMELONG.zip
 	FILESIZE=$(stat -c%s "$FILENAME")
 	echo "Size of $FILENAME = $FILESIZE bytes."
+	rm ../$MREV-$PLATFORM-$CARRIER"-version.txt"
+	exec >>../$MREV-$PLATFORM-$CARRIER"-version.txt" 2>&1
 
 	cd $KERNELDIR
 	echo "Upload zip"
