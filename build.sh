f7b5fbf61498ca23ecdac16a6c8921fde77f6ea3
Jason <jsnmarek@gmail.com>
2013-10-25 15:23:55 -0500

    Fix typo

--- build.sh
+++ build.sh
@@ -101,8 +101,9 @@ time_end=$(date +%s.%N)
         FILESIZE=$(stat -c%s "$FILENAME")
         echo "Size of $FILENAME = $FILESIZE bytes."
         rm ../$MREV-$PLATFORM-$CARRIER"-version.txt"
-        exec >>../$MREV-$PLATFORM-$CARRIER"-version.txt" 2>&1
-	
+        exec >../$MREV-$PLATFORM-$CARRIER"-version.txt" 2>&1
+	echo "$MUXEDNAMELONG,$FILESIZE,SGS4,$PLATFORM,$MUXEDNAMELONG.zip"
+	fi;
 
         cd $KERNELDIR
         ./uploader.sh upload /home/jason/Android/kernel/Packages/$MUXEDNAMELONG.zip /SGS4/$PLATFORM/$MUXEDNAMELONG.zip
