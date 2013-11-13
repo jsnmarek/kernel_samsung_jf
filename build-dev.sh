60eaa147015ddd673614c508ddd65f4af069d5f2
jsnmarek <jsnmarek@gmail.com>
2013-11-13 16:43:27 -0600

    Fix typo

--- build-dev.sh
+++ build-dev.sh
@@ -93,7 +93,7 @@ if [ -e $KERNELDIR/arch/arm/boot/zImage ]; then
         rm ../$MUXEDNAMESHRT.zip > /dev/null 2>&1
         zip -r ../$MUXEDNAMELONG.zip .
 
-        time_end=$(date +%s.%N)
+time_end=$(date +%s.%N)
         echo -e "${BLDYLW}Total time elapsed: ${TCTCLR}${TXTGRN}$(echo "($time_end - $time_start) / 60"|bc ) ${TXTYLW}minutes${TXTGRN} ($(echo "$time_end - $time_start"|bc ) ${TXTYLW}seconds) ${TXTCLR}"
 
         FILENAME=../$MUXEDNAMELONG.zip
