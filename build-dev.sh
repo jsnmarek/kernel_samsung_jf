04264f47f8b2251c8976bb97d439d14c3a81b3fe
jsnmarek <jsnmarek@gmail.com>
2013-11-13 16:11:40 -0600

    Fix build script typo

--- build-dev.sh
+++ build-dev.sh
@@ -102,7 +102,7 @@ if [ -e $KERNELDIR/arch/arm/boot/zImage ]; then
         rm ../$MREV-$PLATFORM-$CARRIER"-version.txt"
         exec >>../$MREV-$PLATFORM-$CARRIER"-version.txt" 2>&1
         echo "$MUXEDNAMELONG,$FILESIZE,http://jsnmarek-VirtualBox/sgs4/$PLATFORM/$MUXEDNAMELONG.zip"
-	
+        
 else
         echo "KERNEL DID NOT BUILD! no zImage exist"
 fi;
